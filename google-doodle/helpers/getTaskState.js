03ce4584d02f87da29a2fee03e97820487422e2b
Raj Prajapati <rajce604@gmail.com>
2023-02-28 11:22:31 -0500

    Task ID from init

--- google-doodle/helpers/getTaskState.js
+++ google-doodle/helpers/getTaskState.js
@@ -1,8 +1,9 @@
 const { Connection, PublicKey } = require("@_koi/web3.js");
+const { TASK_ID } = require("./init");
 async function main() {
   const connection = new Connection("https://k2-testnet.koii.live/");
   const accountInfo = await connection.getAccountInfo(
-    new PublicKey("YOUR TASK ID")
+    new PublicKey(TASK_ID)
   );
   console.log(JSON.parse(accountInfo.data + ""));
 }
