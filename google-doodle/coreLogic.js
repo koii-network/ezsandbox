e141c1b4c578ab9c3e3cbc54453ae83cc3c0912b
Raj Prajapati <rajce604@gmail.com>
2023-04-17 17:31:32 -0400

    distribution list generation changed in google doodle

--- google-doodle/coreLogic.js
+++ google-doodle/coreLogic.js
@@ -50,50 +50,93 @@ class CoreLogic{
   
   }
 
-  async generateDistributionList(round) {
-    console.log("GenerateDistributionList called");
-    console.log("I am selected node");
-  
-    // Write the logic to generate the distribution list here by introducing the rules of your choice
-  
-    /*  **** SAMPLE LOGIC FOR GENERATING DISTRIBUTION LIST ******/
-  
-    let distributionList = {};
-    const taskAccountDataJSON = await namespaceWrapper.getTaskState();
-    const submissions = taskAccountDataJSON.submissions[round];
-    const submissions_audit_trigger =
-      taskAccountDataJSON.submissions_audit_trigger[round];
-    if (submissions == null) {
-      console.log("No submisssions found in N-2 round");
-      return distributionList;
-    } else {
-      const keys = Object.keys(submissions);
-      const values = Object.values(submissions);
-      const size = values.length;
-      console.log("Submissions from last round: ", keys, values, size);
-      for (let i = 0; i < size; i++) {
-        const candidatePublicKey = keys[i];
-        if (
-          submissions_audit_trigger &&
-          submissions_audit_trigger[candidatePublicKey]
-        ) {
-          console.log(
-            submissions_audit_trigger[candidatePublicKey].votes,
-            "distributions_audit_trigger votes "
-          );
-          const votes = submissions_audit_trigger[candidatePublicKey].votes;
-          let numOfVotes = 0;
-          for (let index = 0; index < votes.length; index++) {
-            if (votes[i].is_valid) numOfVotes++;
-            else numOfVotes--;
+  async generateDistributionList(round, _dummyTaskState) {
+    try {
+      console.log('GenerateDistributionList called');
+      console.log('I am selected node');
+
+      // Write the logic to generate the distribution list here by introducing the rules of your choice
+
+      /*  **** SAMPLE LOGIC FOR GENERATING DISTRIBUTION LIST ******/
+
+      let distributionList = {};
+      let distributionCandidates = [];
+      let taskAccountDataJSON = await namespaceWrapper.getTaskState();
+      if (taskAccountDataJSON == null) taskAccountDataJSON = _dummyTaskState;
+      const submissions = taskAccountDataJSON.submissions[round];
+      const submissions_audit_trigger =
+        taskAccountDataJSON.submissions_audit_trigger[round];
+      if (submissions == null) {
+        console.log('No submisssions found in N-2 round');
+        return distributionList;
+      } else {
+        const keys = Object.keys(submissions);
+        const values = Object.values(submissions);
+        const size = values.length;
+        console.log('Submissions from last round: ', keys, values, size);
+
+        // Logic for slashing the stake of the candidate who has been audited and found to be false
+        for (let i = 0; i < size; i++) {
+          const candidatePublicKey = keys[i];
+          if (
+            submissions_audit_trigger &&
+            submissions_audit_trigger[candidatePublicKey]
+          ) {
+            console.log(
+              'distributions_audit_trigger votes ',
+              submissions_audit_trigger[candidatePublicKey].votes,
+            );
+            const votes = submissions_audit_trigger[candidatePublicKey].votes;
+            if (votes.length === 0) {
+              // slash 70% of the stake as still the audit is triggered but no votes are casted
+              // Note that the votes are on the basis of the submission value
+              // to do so we need to fetch the stakes of the candidate from the task state
+              const stake_list = taskAccountDataJSON.stake_list;
+              const candidateStake = stake_list[candidatePublicKey];
+              const slashedStake = candidateStake * 0.7;
+              distributionList[candidatePublicKey] = -slashedStake;
+              console.log('Candidate Stake', candidateStake);
+            } else {
+              let numOfVotes = 0;
+              for (let index = 0; index < votes.length; index++) {
+                if (votes[index].is_valid) numOfVotes++;
+                else numOfVotes--;
+              }
+
+              if (numOfVotes < 0) {
+                // slash 70% of the stake as the number of false votes are more than the number of true votes
+                // Note that the votes are on the basis of the submission value
+                // to do so we need to fetch the stakes of the candidate from the task state
+                const stake_list = taskAccountDataJSON.stake_list;
+                const candidateStake = stake_list[candidatePublicKey];
+                const slashedStake = candidateStake * 0.7;
+                distributionList[candidatePublicKey] = -slashedStake;
+                console.log('Candidate Stake', candidateStake);
+              }
+
+              if (numOfVotes > 0) {
+                distributionCandidates.push(candidatePublicKey);
+              }
+            }
           }
-          if (numOfVotes < 0) continue;
         }
-        distributionList[candidatePublicKey] = 1;
       }
+
+      // now distribute the rewards based on the valid submissions
+      // Here it is assumed that all the nodes doing valid submission gets the same reward
+
+      const reward =
+        taskAccountDataJSON.bounty_amount_per_round /
+        distributionCandidates.length;
+      console.log('REWARD RECEIVED BY EACH NODE', reward);
+      for (let i = 0; i < distributionCandidates.length; i++) {
+        distributionList[distributionCandidates[i]] = reward;
+      }
+      console.log('Distribution List', distributionList);
+      return distributionList;
+    } catch (err) {
+      console.log('ERROR IN GENERATING DISTRIBUTION LIST', err);
     }
-    console.log("Distribution List", distributionList);
-    return distributionList;
   }
   
   async submitDistributionList(round) {
