2d809d88f638bd660424f3b4783d9002e778d33a
Syed Ghazanfer Anwar <s.ghazanferanwar@gmail.com>
2023-05-25 02:13:17 +0500

    Fixed

--- google-doodle/namespaceWrapper.js
+++ google-doodle/namespaceWrapper.js
@@ -55,7 +55,7 @@ class NamespaceWrapper {
   async storeSet(key, value) {
     try {
       await this.initializeDB();
-      await this.db.update({ key: key }, { [key]: value, key }, { upsert: true })
+      await this.#db.update({ key: key }, { [key]: value, key }, { upsert: true })
     } catch (e) {
       console.error(e);
       return undefined;
