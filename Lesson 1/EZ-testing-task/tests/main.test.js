2d06fdb33ebf61d3bee96c9882dc52f52ce7c89b
Laura Abro <laura@koii.network>
2024-07-05 13:56:17 -0300

    Switch to npm namespacewrapper (#68)
    * update package.json
    
    * ignore lock files and node_modules
    
    * change imports to npm package
    
    * remove old namespacewrapper file
    
    * correct namespacewrapper import

--- Lesson 1/EZ-testing-task/tests/main.test.js
+++ Lesson 1/EZ-testing-task/tests/main.test.js
@@ -1,5 +1,5 @@
 const { coreLogic } = require('../coreLogic');
-const { namespaceWrapper, _server } = require('../_koiiNode/koiiNode');
+const { namespaceWrapper, _server } = require('@_koii/namespace-wrapper');
 const Joi = require('joi');
 const axios = require('axios');
 beforeAll(async () => {
