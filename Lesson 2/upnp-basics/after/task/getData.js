ee19c8ea8ab0ee0ab3932c4919e0b5acc70cf29a
Laura Abro <laura@koii.network>
2024-07-08 15:04:40 -0300

    improve error messaging for case where the ip address list is empty or doesn't exist

--- Lesson 2/upnp-basics/after/task/getData.js
+++ Lesson 2/upnp-basics/after/task/getData.js
@@ -6,7 +6,7 @@ class getData {
 
     // get the list of available IP addresses from the task state
     // nodeList is an object with key-value pairs in the form stakingKey: ipAddress
-    const nodeList = taskState.ip_address_list;
+    const nodeList = taskState.ip_address_list ?? {};
 
     // return just the IP addresses
     return Object.values(IPAddressObj);
@@ -16,6 +16,9 @@ class getData {
   }
 
   async getRandomNodeEndpoint(IPAddressArray) {
+    if (!IPAddressArray || IPAddressArray.length === 0) {
+      throw new Error('No IP addresses available');
+    }
    //  Choose a random index
     const randomIndex = Math.floor(Math.random() * IPAddressArray.length);
    //  Return the IP address stored at the random index position
